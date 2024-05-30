b6314c5cdd74ce26eeb0d5b4e15e23f93007ae92
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-05-30 19:36:40 +0200

    feat: add create task service for consume the api endpoint

--- client/src/services/taskService.ts
+++ client/src/services/taskService.ts
@@ -1,5 +1,25 @@
-import axios from 'axios'
+import axios, { isAxiosError} from 'axios'
 import { Task } from '../types/taskTypes'
 
+const client = axios.create({
+  baseURL: process.env.NODE_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
 
-export const createTask = async (task: Task): Promise<Task> => {}
+
+export const createTask = async (task: Task): Promise<Task> => {
+  try {
+    const response = await client.post<Task>('/tasks', task)
+    return response.data
+  } catch (error) {
+    if(isAxiosError(error)) {
+      console.log(error.status)
+      console.error(error.response)
+      throw new Error(error.response?.data.message || 'Failed to create task');
+    } else {
+      throw new Error('Unexpected error occurred')
+    }
+  }
+}
