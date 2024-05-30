b8321ee8b79edc95c893a2ba8475cd657a160d9b
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-05-30 19:24:57 +0200

    fix: implement taskcontroller as a class [sc-276]

--- server/src/infrastructure/express/app.ts
+++ server/src/infrastructure/express/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import { createTaskController } from '../../presentation/controllers/TaskController';
 
 
 const app = express();
 app.use(express.json());
 
-const taskController = new TaskController();
+const taskController = new createTaskController();
 
 app.post('/tasks', (req, res) => taskController.createTask(req, res));
 
