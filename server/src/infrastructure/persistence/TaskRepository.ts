0702eb8318e1a71aeaaca36393b843d63cb4d2d9
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-06-02 20:02:11 +0200

    feat: add getAll method to taskRepository for postgres query on server [sc-279]

--- server/src/infrastructure/persistence/TaskRepository.ts
+++ server/src/infrastructure/persistence/TaskRepository.ts
@@ -1,8 +1,11 @@
 import { Task, TaskProps } from "../../domain/entities/task"
+import { Pool } from 'pg'
 
 
 export class TaskRepository {
   private tasks: Task[] = []
+
+  constructor(private db: Pool) {}
 
   create(taskData: TaskProps): Task {
     const task = new Task({ ...taskData})
@@ -12,5 +15,10 @@ export class TaskRepository {
   async save(task: Task): Promise<Task> {
     this.tasks.push(task)
     return task
+  }
+
+  async getAll(): Promise<Task[]> {
+    const result = await this.db.query('SELECT * FROM tasks')
+    return result.rows.map((task: TaskProps) => new Task(task))
   }
 }
