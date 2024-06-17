1d8006b2d75a81008d58e556c359ae5976a565ce
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-06-17 19:07:33 +0200

    feat: add global error type

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import CreateTask from './pages/CreateTask'
+import GetAllTasks from './pages/GetAllTasks'
 
 
 function App() {
   return (
     <>
      <Routes>
-      <Route path="/" element={<CreateTask />} />
+     <Route path="/" element={<GetAllTasks />} />
+      <Route path="/create-task" element={<CreateTask />} />
      </Routes>
     </>
   )
---
+++ client/src/pages/GetAllTasks.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GetAllTasks = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GetAllTasks
