6e57d1f7e1b2ecf121c0e5223ab03df552bb5983
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-06-19 15:48:27 +0200

    feat: add component for get all tasks on client [sc-269]

--- client/src/pages/GetAllTasks.tsx
+++ client/src/pages/GetAllTasks.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Task } from '../types/taskTypes'
+import { getAllTasks } from '../services/taskService'
 
 const GetAllTasks = () => {
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const tasksData = await getAllTasks();
+        setTasks(tasksData);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    };
+    fetchTasks();
+  }, [])
+
   return (
     <div>
-      
+      <h2>All Tasks</h2>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
