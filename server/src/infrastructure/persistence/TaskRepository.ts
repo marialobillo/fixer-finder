ae6d9f727929eaf33e9826f2b37d4c992604d013
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-07-05 10:51:57 +0200

    feat: add params for tags and search to getAll in TaskRepository interface sc-293

--- server/src/infrastructure/persistence/TaskRepository.ts
+++ server/src/infrastructure/persistence/TaskRepository.ts
@@ -3,5 +3,5 @@ import { Task, TaskProps } from "../../domain/entities/task"
 
 export interface TaskRepository {
   create(taskData: TaskProps): Promise<Task>
-  getAll(): Promise<Task[]> 
+  getAll(tags?: string[], search?: string): Promise<Task[]> 
 }
