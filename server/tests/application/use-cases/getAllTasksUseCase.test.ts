97f72a2005e474569ee24edeb50d8c07e1809c34
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-07-08 09:33:43 +0200

    test: add should return tasks filtered by tags and search sc-293

--- server/tests/application/use-cases/getAllTasksUseCase.test.ts
+++ server/tests/application/use-cases/getAllTasksUseCase.test.ts
@@ -78,4 +78,19 @@ describe('Get All Tasks Use Case', () => {
     expect(tasks[0].title).toBe('Task 1')
   })
 
+  it('should return filtered tasks by tags and search attributes', async () => {
+    const expectedTasks = [
+      { ...taskProps, id: '1', title: 'Task 1', tags: ['tag1', 'tag2'] },
+      { ...taskProps, id: '2', title: 'Task 2', tags: ['tag2', 'tag3'] },
+    ]
+    taskRepository.getAll.mockResolvedValue(expectedTasks)
+
+    const params: FetchTasksParams = { tags: 'tag2', search: 'Task 1' }
+    const tasks = await getAllTasksUseCase.execute(params)
+
+    expect(tasks.length).toBe(2)
+    expect(tasks[0].title).toBe('Task 1')
+    expect(tasks[0].tags).toContain('tag2')
+  })
+
 })
