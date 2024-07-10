1c38d6b7cfa8bac53f178f191694bdb9ea937560
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-07-10 13:11:49 +0200

    fix: change name of create-task for newTask sc-293

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import CreateTask from './pages/CreateTask'
 import GetAllTasks from './pages/GetAllTasks'
 import GetTasksFiltered from './pages/GetTasksFiltered'
+import NewTask from './pages/NewTask'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<GetAllTasks />} />
-        <Route path='/create-task' element={<CreateTask />} />
+        <Route path='/newtask' element={<NewTask />} />
         <Route path='/tasks/filter' element={<GetTasksFiltered />} />
       </Routes>
     </>
