ee35796600f7803db3fc482f090c1d9ee299d93f
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-08-01 12:35:25 +0200

    feat: add the register user endpoint on app sc-321

--- server/src/infrastructure/express/app.ts
+++ server/src/infrastructure/express/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { TaskController } from '../../presentation/controllers/TaskController';
 import cors from 'cors';
 import { OfferController } from '../../presentation/controllers/OfferController';
+import { AuthController } from '../../presentation/controllers/AuthController';
 
 const app = express();
 app.use(cors());
@@ -9,10 +10,13 @@ app.use(express.json());
 
 const taskController = new TaskController();
 const offerController = new OfferController();
+const authController = new AuthController();
 
 app.post('/tasks', (req, res) => taskController.createTask(req, res));
 app.get('/tasks', (req, res) => taskController.getAllTask(req, res));
 
 app.post('/offers', (req, res) => offerController.createOffer(req, res));
+
+app.post('/regiser', (req, res) => authController.registerUser(req, res));
 
 export { app };
